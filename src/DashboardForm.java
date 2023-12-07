a808c38eabc006ca1b786b967fdea796e11b6ccd
ejclayson <109399030+ejclayson@users.noreply.github.com>
2023-12-07 23:53:44 +0800

    Accomplished Job Sheet 2 - 3 Cash - in 202312072353

--- src/DashboardForm.java
+++ src/DashboardForm.java
@@ -12,6 +12,7 @@ public class DashboardForm extends JFrame{
     private JPanel dashboardPanel;
     private JButton changePinButton;
     private JLabel lblAmount;
+    private JButton cashInButton;
 
     private String name;
 
@@ -41,28 +42,95 @@ public class DashboardForm extends JFrame{
         changePinButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String m = JOptionPane.showInputDialog("Please input new pin?");
-                final String DB_URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12666768";
-                final String USERNAME = "sql12666768";
-                final String PASSWORD = "YxDac3ZBu9";
+                String m = JOptionPane.showInputDialog("Please input new pin:");
+                if(!m.matches("^\\d+$")){
+                    JOptionPane.showMessageDialog(null,"Pin must contain valid numbers only", "Try again", JOptionPane.ERROR_MESSAGE);
+                }else if(m.length()!=4){
+                    JOptionPane.showMessageDialog(null,"Pin must contain four(4) numbers", "Try again", JOptionPane.ERROR_MESSAGE);
+                }else {
+                    final String DB_URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12666768";
+                    final String USERNAME = "sql12666768";
+                    final String PASSWORD = "YxDac3ZBu9";
 
-                try(Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);){
+                    try(Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);){
 
-                    PreparedStatement preparedStatement = conn.prepareStatement("update users set pin = ? where name = ?");
-                    preparedStatement.setString(1, m);
-                    preparedStatement.setString(2, name);
-                    preparedStatement.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Pin is now updated to " + m + "\nYou'll now be logout");
-                    dashboardPanel.setVisible(false);
-                    dispose();
-                    LoginForm myLoginForm = new LoginForm(null);
-                    myLoginForm.setVisible(true);
-                }catch (SQLException e1){
-                    e1.printStackTrace();
+                        PreparedStatement preparedStatement = conn.prepareStatement("update users set pin = ? where name = ?");
+                        preparedStatement.setString(1, m);
+                        preparedStatement.setString(2, name);
+                        preparedStatement.executeUpdate();
+                        JOptionPane.showMessageDialog(null, "Pin is now updated to " + m + "\nYou'll now be logout");
+                        dashboardPanel.setVisible(false);
+                        dispose();
+                        LoginForm myLoginForm = new LoginForm(null);
+                        myLoginForm.setVisible(true);
+                    }catch (SQLException e1){
+                        e1.printStackTrace();
+                    }
                 }
             }
+
         });
 
+        cashInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Double m = Double.valueOf(JOptionPane.showInputDialog("Please input cash in amount:"));
+                Double m = Double.parseDouble(JOptionPane.showInputDialog("Please input cash in amount:"));
+                if(!m.toString().matches("^[\\+\\-]{0,1}[0-9]+[\\.\\,][0-9]+$")){
+                    JOptionPane.showMessageDialog(null,"Please input a valid number for the amount", "Try again", JOptionPane.ERROR_MESSAGE);
+                }else if(m<=100.00){
+                    JOptionPane.showMessageDialog(null,"Please input a valid number higher than 100 for amount", "Try again", JOptionPane.ERROR_MESSAGE);
+                } else{
+                    final String DB_URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12666768";
+                    final String USERNAME = "sql12666768";
+                    final String PASSWORD = "YxDac3ZBu9";
+                    try (Connection con = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);){
+                        PreparedStatement ps = con.prepareStatement("SELECT id FROM users WHERE name=?");
+                        ps.setString(1,name);
+                        ResultSet rs = ps.executeQuery();
+                        if(rs.next()){
+                            String userId = rs.getString(1);
+                            PreparedStatement ps2 = con.prepareStatement("SELECT amount FROM balance WHERE user_id=?");
+                            ps2.setString(1,userId);
+                            ResultSet rs2 = ps2.executeQuery();
+                            if(rs2.next()){
+                                double balanceAmount = Double.parseDouble(rs2.getString(1));
+                                try(Connection con2 = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);){
+                                    PreparedStatement ps3 = con2.prepareStatement("INSERT INTO transaction (amount,name,account_id,date,transfertoid,transferfromid) values (?,?,?,?,?,?)");
+                                    ps3.setDouble(1, m);
+                                    ps3.setString(2, name);
+                                    ps3.setString(3, userId);
+                                    ps3.setString(4, String.valueOf(new Timestamp(System.currentTimeMillis())));
+                                    ps3.setString(5, userId);
+                                    ps3.setString(6, userId);
+                                    ps3.executeUpdate();
+                                    PreparedStatement ps4 = con2.prepareStatement("UPDATE balance SET amount = ? WHERE user_id = ?");
+                                    ps4.setDouble(1, balanceAmount + m);
+                                    ps4.setString(2, userId);
+                                    ps4.executeUpdate();
+                                    try(Connection con3 = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);){
+                                        PreparedStatement ps5 = con3.prepareStatement("select amount from balance where user_id=?");
+                                        ps5.setString(1,userId);
+                                        ResultSet rs3 = ps2.executeQuery();
+                                        if(rs3.next()){
+                                            String amount = rs3.getString(1);
+                                            lblAmount.setText("Your current balance is: Php " + amount);
+                                        }
+                                    }
+                                }catch (Exception e2){
+                                    e2.printStackTrace();
+                                }
+                            }
+                        }
+                    }catch (Exception e1){
+                        e1.printStackTrace();
+                    }
+                }
+
+
+
+            }
+        });
 
         logoutButton.addActionListener(new ActionListener() {
             @Override
@@ -75,6 +143,7 @@ public class DashboardForm extends JFrame{
                 myLoginForm.setVisible(true);
             }
         });
+
     }
 
 
@@ -106,7 +175,6 @@ public class DashboardForm extends JFrame{
                     }
                 }catch (SQLException e){
                     if (e.getErrorCode() == 1062) {
-//                        JOptionPane.showMessageDialog(this,"Name and/or Email and/or Mobile already registered\nPlease register another Name, Email, and Mobile.", "Try again", JOptionPane.ERROR_MESSAGE);
                         try(Connection con2 = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);){
                             PreparedStatement ps2 = con2.prepareStatement("select amount from balance where user_id=?");
                             ps2.setString(1,userId);
